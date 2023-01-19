14619d1bf45bfb4186da1c65f6904cdedbe7c074
금서하 <048965s@naver.com>
2023-01-19 19:09:22 +0900

    230119

--- mdti-app/src/components/Result.jsx
+++ mdti-app/src/components/Result.jsx
@@ -4,6 +4,29 @@ import BlueButton from './BlueButton'
 import { useSelector ,useDispatch } from 'react-redux';
 import { reset } from '../store/modules/mbti';
 
+const Header = styled.p`
+font-size : 3em;
+`;
+
+const Explaination = styled.p`
+font-size : 1.5em;
+color: #777;
+`;
+
+const ResultTag = styled.p`
+font-size: 3em;
+color: dodgerblue;
+`;
+
+const Additional = styled.p`
+font-size : 2em;
+color: orange;
+`;
+
+const AdditionalImg = styled.img`
+width: 500px;
+transform: translateX(-35px);
+`;
 
 export default function Result() {
     const result = useSelector((state) => state.mbti.mbtiResult);
@@ -11,29 +34,6 @@ export default function Result() {
     const dispatch = useDispatch();
 
 
-    const Header = styled.p`
-        font-size : 3em;
-    `;
-
-    const Explaination = styled.p`
-        font-size : 1.5em;
-        color: #777;
-    `;
-
-    const Result = styled.p`
-        font-size: 3em;
-        color: dodgerblue;
-    `;
-
-    const Additional = styled.p`
-        font-size : 2em;
-        color: orange;
-    `;
-
-    const AdditionalImg = styled.img`
-        width: 500px;
-        transform: translateX(-35px);
-    `;
 
     
 
@@ -41,7 +41,7 @@ export default function Result() {
     <>
     <Header>당신의 개발자 MBTI 결과는? </Header>
     <Explaination>{explaination.text}</Explaination>
-    <Result>{result}</Result>
+    <ResultTag>{result}</ResultTag>
     <Additional>이건 재미로 읽어 보세요! </Additional>
     <AdditionalImg src={explaination.img}alt="팩폭"/>
     <BlueButton text="다시 검사하기" clickEvent={() => dispatch(reset())}/>
